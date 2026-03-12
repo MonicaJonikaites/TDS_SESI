c958d46b81994691f33be6e8b56a53adb7bf8a68
MonicaJonikaites <monicacjonikaites@gmail.com>
2026-03-12 12:12:41 -0300

    Estruturas de controle

---
+++ controle/else_if.js
@@ -0,0 +1,14 @@
+// Desafio 
+let n1 = 6;
+let n2 = 6;
+let media = 7.5;
+
+media = (n1 + n2) /2;
+
+if (media >= 7.5) {
+    console.log("Aprovado!");
+} else if (media < 7.5 && media >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
---
+++ controle/template_string.js
@@ -0,0 +1,5 @@
+let nome = "Monica Jonikaites";
+let idade = 15;
+// Imprima no terminal o nome e a idade junto com uma frase.
+console.log("Meu nome é " + nome + ", e tenho " + idade + " anos de idade");
+    console.log(`Meu nome é  ${nome} e tenho  ${idade}  anos de idade`);
