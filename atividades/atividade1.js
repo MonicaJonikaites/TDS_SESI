195e40b682d311cbc795a03925a19bfdf143afcf
MonicaJonikaites <monicacjonikaites@gmail.com>
2026-03-26 11:38:32 -0300

    Atividade 1

---
+++ atividades/atividade1.js
@@ -0,0 +1,11 @@
+let produto = "Shampoo";
+let preco = 25.0;
+let estoque = true
+
+ console.log(`O item ${produto} custa ${preco}`);
+ 
+ if (estoque >= true){
+    console.log("Produto indisponivel");
+ } else {
+    console.log("Produto disponivel");
+ }
