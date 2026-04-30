6e5e2ba6e4cbc1abf458375c50e74db4fc2c6e6a
MonicaJonikaites <monicacjonikaites@gmail.com>
2026-04-30 11:24:54 -0300

    Update array2.js

--- array/array2.js
+++ array/array2.js
@@ -10,3 +10,10 @@ comsole.log(nome.length);
  console.log(nome);
 
 // Desafio: inserior 5 novos indices com 5 nomes e imprimir na tela
+
+nomes[0] = "Monica";
+nomes[1] = "Nicoly";
+nomes[2] = "Karine";
+nomes[3] = "Sther";
+nomes[4] = "Ana";
+console.log(nomes);
